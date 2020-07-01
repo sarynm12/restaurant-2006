f7e3cf8042138a86644976f761f8680c1d79ea3f
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-01 13:24:15 -0600

    closing time method working

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,4 +1,14 @@
 class Restaurant
   attr_reader :name, :opening_time, :dishes
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hours)
+    closing = (@opening_time.to_i + hours)
+    "#{closing}:00"
+  end
 end
